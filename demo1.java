5582b1ac19aec4b03ed1a2ad120a020f24e7d0e5
Yogesh Bhosale <bhosaleyogesh0292@gmail.com>
2023-09-02 10:56:17 +0530

    variable

---
+++ demo1.java
@@ -0,0 +1,25 @@
+package Variable;
+
+public class demo1 {
+   public static void main(String[] args) {
+	
+	   //step1: variable declaration 
+	   String sname;
+	   int srollNum; 
+	   char sgrade; 
+	   float sper;
+	   //step2: variable initialization 
+	   sname = "swapnil";
+	   srollNum = 10;
+	   sgrade ='A'; 
+	   sper= 65.5f;
+	   //step3: usage
+	   System.out.println("Student name: "+sname);
+	   System.out.println("Student roll num: "	+ srollNum); 
+	   System.out.println("Student grade: "+sgrade); 
+	   System.out.println("Student percentage: "+sper +"%");
+
+	   
+	   
+}
+}
